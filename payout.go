5c543d8de8474084c29eb53568e2bccd26eb751d
Igor A. Melekhine <igor.melekhine@me.com>
2020-08-18 19:31:36 +0500

    amount in payout

--- payout.go
+++ payout.go
@@ -4,9 +4,11 @@ package themap
 // orderid original orderid used in Charge or Pay methods
 // card parameter is a cardno or shadow card representation
 // second paramenter is pan flag, if true we treat first prameter as actual card number
-func (p *Payment) Payout(orderid, card string, ispan bool) error {
+func (p *Payment) Payout(amount int, orderid, card string, ispan bool) error {
 
 	var err error
+
+	p.Amount = amount
 
 	if ispan {
 		p.Card = Card{PAN: card}
