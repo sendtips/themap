3d7868f2e82349f9e7c38d00e8928f2d40d99b65
Igor A. Melekhine <igor.melekhine@me.com>
2020-09-10 19:08:36 +0500

    ApplePay block for testing

--- applepay.go
+++ applepay.go
@@ -22,3 +22,22 @@ func (p *Payment) ApplePay(ctx context.Context, amount int, token []byte) error 
 	return err
 
 }
+
+// ApplePayBlock blocks
+// NOT FOR PRODUCTION
+func (p *Payment) ApplePayBlock(ctx context.Context, amount int, token []byte) error {
+	var err error
+
+	p.ApplePayToken = base64.StdEncoding.EncodeToString(token)
+	p.Amount = amount
+
+	err = proceedRequest(ctx, "POST", "/Block", p)
+	if err != nil {
+		return err
+	}
+
+	err = p.checkErrors()
+
+	return err
+
+}
